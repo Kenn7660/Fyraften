ade913f8a7654963310e4dd799b13a7f73d620ee
Daniel Kringelum <Daniel Kringelum>
2017-03-14 11:20:14 +0100

    InfoWindow virker

--- Script.js
+++ Script.js
@@ -7,27 +7,27 @@ function initMap() {
         zoom: 13,
         center: myCenter
     });
+createMarker();
 }
 
 function createMarker(){
+    console.log("Lav marker");
     var marker = new google.maps.Marker({
         position: {lat: 55.683295, lng: 12.571506},
         map: map,
         title: "Marker",
     });
 
-//    var infoWindow = new google.maps.InfoWindow({
-//        content: "Bliver overskrevet",
-//        position: info.position
-//    });
-//
-//    marker.addListener("click", function(){
-//        var klon = document.querySelector("#template_indhold").content.cloneNode(true);
-//        klon.querySelector(".navn").textContent = info.navn;
-//        klon.querySelector(".beskrivelse").textContent = info.beskrivelse;
-//        klon.querySelector(".billede").src = info.billede;
-//        infoWindow.setContent(klon);
-//        infoWindow.open(map);
-//    });
+    var infoWindow = new google.maps.InfoWindow({
+        content: "Bliver overskrevet",
+        position: {lat: 55.683295, lng: 12.571506},
+    });
+
+    marker.addListener("click", function(){
+        var klon = document.querySelector("#info").content.cloneNode(true);
+        klon.querySelector(".navn").textContent = "Marker ved Nørreport";
+        infoWindow.setContent(klon);
+        infoWindow.open(map);
+    });
 
 }
