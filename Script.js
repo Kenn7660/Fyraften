22de3f4ff4bd4b2ce5a6e8ee1f6c69012643ef6e
Daniel Kringelum <Daniel Kringelum>
2017-03-14 11:12:49 +0100

    Template test

--- Script.js
+++ Script.js
@@ -8,3 +8,26 @@ function initMap() {
         center: myCenter
     });
 }
+
+function createMarker(){
+    var marker = new google.maps.Marker({
+        position: {lat: 55.683295, lng: 12.571506},
+        map: map,
+        title: "Marker",
+    });
+
+//    var infoWindow = new google.maps.InfoWindow({
+//        content: "Bliver overskrevet",
+//        position: info.position
+//    });
+//
+//    marker.addListener("click", function(){
+//        var klon = document.querySelector("#template_indhold").content.cloneNode(true);
+//        klon.querySelector(".navn").textContent = info.navn;
+//        klon.querySelector(".beskrivelse").textContent = info.beskrivelse;
+//        klon.querySelector(".billede").src = info.billede;
+//        infoWindow.setContent(klon);
+//        infoWindow.open(map);
+//    });
+
+}
