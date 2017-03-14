132c98770bfad6d746d4b61a35706e9b0ab2a6e7
Daniel Kringelum <Daniel Kringelum>
2017-03-14 12:49:24 +0100

    Markers loaded

--- Script.js
+++ Script.js
@@ -9,25 +9,29 @@ function initMap() {
         styles: [{"featureType":"all","elementType":"all","stylers":[{"saturation":"-29"},{"lightness":"0"},{"hue":"#ff8d00"}]},{"featureType":"administrative.country","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"administrative.province","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"administrative.locality","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"administrative.neighborhood","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"administrative.land_parcel","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"lightness":"0"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"saturation":"-50"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"saturation":"5"}]},{"featureType":"landscape.natural.landcover","elementType":"geometry","stylers":[{"saturation":"-2"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"saturation":"-17"}]},{"featureType":"landscape.natural.terrain","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"saturation":"-64"},{"lightness":"-8"}]},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"lightness":"-40"},{"saturation":"-61"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"weight":"1.5"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#c1bdb5"}]}]
                 });
 
-createMarker();
+    $.getJSON("fyraften.JSON", importData);
 }
 
-function createMarker(){
+function importData(JSONdata){
+    JSONdata.forEach(createMarker);
+}
+
+function createMarker(info){
     console.log("Lav marker");
     var marker = new google.maps.Marker({
-        position: {lat: 55.683295, lng: 12.571506},
+        position: info.position,
         map: map,
-        title: "Marker",
+        title: info.navn,
     });
 
     var infoWindow = new google.maps.InfoWindow({
         content: "Bliver overskrevet",
-        position: {lat: 55.683295, lng: 12.571506},
+        position: info.psotion,
     });
 
     marker.addListener("click", function(){
         var klon = document.querySelector("#info").content.cloneNode(true);
-        klon.querySelector(".navn").textContent = "Marker ved Nørreport";
+        klon.querySelector(".navn").textContent = "Marker template";
         infoWindow.setContent(klon);
         infoWindow.open(map);
     });
